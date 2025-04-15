4b150ead5be68934379d242a9c842ac8c3cea9c7
MARTINA ALEJANDRA MONTESDEOCA LUZURIAGA <m.montesdeoca@debian.dit.upm.es>
2025-04-15 13:21:29 +0200

    Actualizado solo funcion crear con comentarios

--- pfinal1.py
+++ pfinal1.py
@@ -9,21 +9,23 @@ import os
 
 ARCHIVO_CONFIG = "servidores.txt"
 
-# Verificar argumentos
+# Verificar si se ha proporcionado un nombre de contenedor
 if len(sys.argv) < 2:
     print("Uso: python3 pfinal1.py <create/start/list/delete> [num_servidores]")
     sys.exit(1)
 
-accion = sys.argv[1]
+accion = sys.argv[1]  # esto va a ser el primer argumento del comando
 
 def crear_escenario(n):
     with open(ARCHIVO_CONFIG, "w") as file:
-        for i in range(1, n + 1):
+        for i in range(1, n + 1): # dentro del bucle for se crean n contenedores. Si no ponemos +1 carga hasta el 3 si ponemos '4'
             nombre = f"s{i}"
             print(f"Creando servidor {nombre}...")
             subprocess.run(["lxc", "init", "ubuntu:20.04", nombre])
+            # Esto es para guardar si se creó con éxito...
             file.write(nombre + "\n")
 
+        # Fuera del bucle se crean contendores como balanceador y cliente
         print("Creando balanceador lb...")
         subprocess.run(["lxc", "init", "ubuntu:20.04", "lb"])
         file.write("lb\n")
