70abcc1bdc0b74e614f4ef54f27931ba250dfdbd
Lucia Garcia <xiaoshu.lucia@gmail.com>
2025-04-30 12:32:38 +0200

    mejoras lab 1

--- delete_escenario.py
+++ delete_escenario.py
@@ -12,14 +12,33 @@ ARCHIVO_CONFIG = "servidores.txt"
 #     if ans == "Y":
 #         return true
    
-
+   
 
 
 def delete():
     with open(ARCHIVO_CONFIG, "r") as file:
-        cont = [line.strip() for line in file.readlines()]
-        subprocess.run(["lxc", "delete", "--force"])
-        # subprocess.run(["lxc", "network", "delete", "lxdbr0"])
-        subprocess.run(["lxc", "network", "delete", "lxdbr1"])
+        contenedores = [line.strip() for line in file.readlines()]
 
-        ARCHIVO_CONFIG.write(" ")
+    while True:
+        ans = input("Si desea borrar todos los contenedores y bridges escriba TODOS.\n" 
+                    "Si desea borrar un contenedor individual escriba el nombre: \n").strip()
+        if ans == "TODO" or ans =="todo" or ans == "Todo":
+            for c in contenedores:
+                subprocess.run(["lxc", "delete", c, "--force"])
+            subprocess.run(["lxc", "network", "delete", "lxdbr0"])
+            subprocess.run(["lxc", "network", "delete", "lxdbr1"])
+            with open(ARCHIVO_CONFIG, "w") as file:
+                file.write("")
+
+        elif ans in contenedores:
+            subprocess.run(["lxc", "delete", ans, "--force"])
+            # eliminarlo de la lista contenedores (esta en memoria)
+            contenedores.remove(ans)
+            # eliminar del archivo
+            with open(ARCHIVO_CONFIG, "w") as file:
+                file.write("\n".join(contenedores))
+
+        else:
+            print("Nombre no válido. Intente de nuevo")
+        
+    ARCHIVO_CONFIG.write(" ")   
